ad1bf686ae8845d89fbeeed8c1446b46a44237ec
David Day <david.day@utexas.edu>
2022-07-06 00:19:18 +0000

    not using install script

--- .install.sh
+++
@@ -1,4 +0,0 @@
-git clone --bare git@github.com:davidday99/dotfiles.git $HOME/.cfg
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-config checkout
-config config --local status.showUntrackedFiles no
