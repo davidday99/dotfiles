2fb151edc607f8b6887d924eadf177d608b72ffb
David Day <david.day2017@gmail.com>
2022-07-05 22:07:56 -0500

    add readme and installation script

---
+++ .install.sh
@@ -0,0 +1,4 @@
+git clone --bare git@github.com:davidday99/dotfiles.git $HOME/.cfg
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+config checkout
+config config --local status.showUntrackedFiles no
