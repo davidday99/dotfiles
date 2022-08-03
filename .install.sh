c9b137d1fc96bada16a395593c361bba345a1a79
David Day <david.day2017@gmail.com>
2022-08-02 23:58:36 -0500

    readd install script and remove readme

---
+++ .install.sh
@@ -0,0 +1,6 @@
+# Installation script
+
+git clone --bare git@github.com:davidday99/dotfiles.git $HOME/.cfg
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+config checkout
+config config --local status.showUntrackedFiles no
