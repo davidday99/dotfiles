ca563e1f43ef2c9e5d5d0c546637ab53a664aee7
David Day <david.day2017@gmail.com>
2022-08-03 00:09:29 -0500

    use tinyurl for install.sh

--- .install.sh
+++ .install.sh
@@ -1,10 +1,7 @@
 # Installation script for configurationn
 # taken from https://www.atlassian.com/git/tutorials/dotfiles.
 # Download this script using: 
-
-# curl -fsSL \
-# https://raw.githubusercontent.com/davidday99/dotfiles/main/.install.sh \
-# -o install.sh
+# curl -fsSL https://tinyurl.com/ddaydotfiles -o install.sh
 
 git clone --bare git@github.com:davidday99/dotfiles.git $HOME/.cfg
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
