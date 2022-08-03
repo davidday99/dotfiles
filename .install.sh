612b7ea8f23bcd7cde5ccc8969d27b0bbf21c49a
David Day <david.day2017@gmail.com>
2022-08-03 00:05:11 -0500

    add instructions to install script

--- .install.sh
+++ .install.sh
@@ -1,4 +1,10 @@
-# Installation script
+# Installation script for configurationn
+# taken from https://www.atlassian.com/git/tutorials/dotfiles.
+# Download this script using: 
+
+# curl -fsSL \
+# https://raw.githubusercontent.com/davidday99/dotfiles/main/.install.sh \
+# -o install.sh
 
 git clone --bare git@github.com:davidday99/dotfiles.git $HOME/.cfg
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
